d4c17eb9aef123fda429d0c316d39a3fd3439945
MingweiZhu666 <mzhu@aviatrix.com>
2022-08-05 13:06:59 -0700

    AVX-26997 update terraform aviatrix OCI module source url and output description

--- modules/aviatrix-controller-build/output.tf
+++ modules/aviatrix-controller-build/output.tf
@@ -1,11 +1,14 @@
 output "aviatrix_controller_public_ip" {
+  description = "Controller public ip address."
   value = oci_core_instance.simple-vm.public_ip
 }
 
 output "aviatrix_controller_private_ip" {
+  description = "Controller private ip address."
   value = oci_core_instance.simple-vm.private_ip
 }
 
 output "aviatrix_controller_url" {
+  description = "Controller URL."
   value = "https://${oci_core_instance.simple-vm.public_ip}"
 }
